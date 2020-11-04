b82de77cc621fc1da9e277a617d44ae32be52c13
__xjb__ <chrisbobek@gmail.com>
2020-11-04 02:00:46 -0800

    Update linter.js

--- linter.js
+++ linter.js
@@ -1,8 +1,19 @@
 // Display the word counts for each overused word
-const story = "The super quick crazy lazy brown fox seriously jumped bigly over the super seriously lazy crazy white rabbit.";
+const story = 'The super quick crazy lazy brown fox seriously jumped bigly over the super seriously lazy crazy white rabbit.';
 const storyWords = story.split(' ');
-const overusedWords = ['super', 'crazy', 'seriously']
+const overusedWords = ['super', 'crazy', 'seriously', 'lazy', 'stupid'];
 
-function countOverusedWords() {
-    //
+function countOverusedWords(storyWordsArray, overusedWordsArray) {
+    for (let i = 0; i < overusedWordsArray.length; i++) {
+        let counter = 0;
+        let index = storyWordsArray.indexOf(overusedWordsArray[i]);
+        while (index > -1) {
+            counter++;
+            index = storyWordsArray.indexOf(overusedWordsArray[i], index);
+        }
+        console.log(`${overusedWordsArray[i]} occurs ${counter} times.`);
+    }
+
 }
+
+countOverusedWords(storyWords, overusedWords);
