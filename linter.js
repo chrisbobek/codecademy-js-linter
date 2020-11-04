639dd26c34513ff9be6f1bac74fd3a3dfa86222a
__xjb__ <chrisbobek@gmail.com>
2020-11-03 22:07:41 -0800

    Adds string and converts string to an array.

---
+++ linter.js
@@ -0,0 +1,7 @@
+// Display the word counts for each overused word
+story = "The quick crazy brown fox seriously jumped bigly over the super lazy white rabbit.";
+storyWords = story.split(' ');
+
+function overusedWords() {
+    //
+}
