04fdb463dc9ccb747c1786e3490ef3c5693749f3
__xjb__ <chrisbobek@gmail.com>
2020-11-03 22:55:32 -0800

    Renames function; adds overusedWords array; modifies story string

--- linter.js
+++ linter.js
@@ -1,8 +1,8 @@
 // Display the word counts for each overused word
-const story = "The super quick crazy brown fox seriously jumped bigly over the super seriously lazy crazy white rabbit.";
+const story = "The super quick crazy lazy brown fox seriously jumped bigly over the super seriously lazy crazy white rabbit.";
 const storyWords = story.split(' ');
-const overusedWords = ['crazy', 'super', 'seriously'];
+const overusedWords = ['super', 'crazy', 'seriously']
 
-function overusedWords() {
+function countOverusedWords() {
     //
 }
